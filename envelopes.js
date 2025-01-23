d22de827e4036b523d73aa2ee1515651965f4862
simo-123123 <185842307+simo-123123@users.noreply.github.com>
2025-01-23 16:23:18 +0100

    Add GET /api/envelopes route

--- envelopes.js
+++ envelopes.js
@@ -1,4 +1,14 @@
 const express = require('express');
 const envelopesRouter = express.Router();
 
+const { getAllEnvelopes,
+    getSingleEnvelope,
+    createEnvelope,
+    updateEnvelope,
+    deleteEnvelope } = require('./db-utils/db-functions');
+
+envelopesRouter.get('/', (req, res) => {
+    res.json({ envelopes: getAllEnvelopes() })
+});
+
 module.exports = envelopesRouter;
