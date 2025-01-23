511d20a4b53e0f1e2692b1f726c97947c7a40006
simo-123123 <185842307+simo-123123@users.noreply.github.com>
2025-01-23 17:35:11 +0100

    Add POST /api/envelopes route

--- envelopes.js
+++ envelopes.js
@@ -21,11 +21,16 @@ envelopesRouter.param('id', (req, res, next, id) => {
 });
 
 envelopesRouter.get('/', (req, res) => {
-    res.json({ envelopes: getAllEnvelopes() })
+    res.json({ envelopes: getAllEnvelopes() });
 });
 
 envelopesRouter.get('/:id', (req, res) => {
     res.json({ envelope: req.envelope });
+});
+
+envelopesRouter.post('/', validateEnvelope, (req, res) => {
+    const createdEnvelope = createEnvelope(req.newEnvelope);
+    res.status(201).json({ envelope: createdEnvelope });
 });
 
 module.exports = envelopesRouter;
