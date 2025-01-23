a12c08ea79967b2438da3332344f5b107143123d
simo-123123 <185842307+simo-123123@users.noreply.github.com>
2025-01-23 16:28:19 +0100

    Add id router parameter

--- envelopes.js
+++ envelopes.js
@@ -7,6 +7,17 @@ const { getAllEnvelopes,
     updateEnvelope,
     deleteEnvelope } = require('./db-utils/db-functions');
 
+envelopesRouter.param('id', (req, res, next, id) => {
+    const envelope = getSingleEnvelope(id);
+
+    if (!envelope) {
+        return res.sendStatus(404);
+    }
+
+    req.envelope = envelope;
+    next();
+}) 
+
 envelopesRouter.get('/', (req, res) => {
     res.json({ envelopes: getAllEnvelopes() })
 });
