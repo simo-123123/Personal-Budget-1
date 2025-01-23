3230abe8ed3589ba00b5b96f1a2989c23dbc46df
simo-123123 <185842307+simo-123123@users.noreply.github.com>
2025-01-23 16:10:59 +0100

    Set up dotenv

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+require('dotenv').config();
+
 if (process.env.NODE_ENV === 'development') {
     const morgan = require('morgan');
     app.use(morgan('dev'));
